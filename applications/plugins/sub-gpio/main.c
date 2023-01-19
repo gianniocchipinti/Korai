7167145ab81d2412b1e04aa12394f543729acbbc
ankris812 <62661743+ankris812@users.noreply.github.com>
2023-01-19 17:32:18 +0100

    Update main.c

--- applications/plugins/sub-gpio/main.c
+++ applications/plugins/sub-gpio/main.c
@@ -6,6 +6,20 @@
 #include "fatfs.h"
 #include "FreeRTOS.h"
 
+// Pin definitions for the CC1101
+#define CC1101_CS_GPIO_Port GPIOC
+#define CC1101_CS_Pin GPIO_PIN_0
+#define CC1101_MOSI_GPIO_Port GPIOA
+#define CC1101_MOSI_Pin GPIO_PIN_7
+#define CC1101_MISO_GPIO_Port GPIOA
+#define CC1101_MISO_Pin GPIO_PIN_6
+#define CC1101_SCK_GPIO_Port GPIOA
+#define CC1101_SCK_Pin GPIO_PIN_5
+#define CC1101_GDO0_GPIO_Port GPIOC
+#define CC1101_GDO0_Pin GPIO_PIN_1
+
+SPI_HandleTypeDef hspi1;
+
 // Global variables for storing the selected frequency, file name, and recording status
 int frequency;
 char file_name[256];
@@ -15,15 +29,47 @@ bool recording;
 void select_frequency();
 void send_file();
 void read_signals();
-void save_signals();
+void save_signals(char *buffer, int bytes_read);
+int get_user_input();
+int cc1101_init();
+int stm32wb55_init();
+void u8g2_init(u8g2_t *u8g2, ...);
+void u8g2_clear_buffer(u8g2_t *u8g2);
+void u8g2_draw_string(u8g2_t *u8g2, int x, int y, const char *str);
+void u8g2_send_buffer(u8g2_t *u8g2);
+void cc1101_set_frequency(int frequency);
+void cc1101_transmit(char *data, int length);
+int cc1101_receive();
+FRESULT select_file_from_storage(DIR *dir, FILINFO *fno, char *file_name);
+int delay(int ms);
 
 int main()
 {
-    // Initialize the CC1101 chip and STM32WB55
-    cc1101_init();
+    // Initialize the CC1101 chip
+    if (!cc1101_init())
+    {
+        // Display a message to the user indicating that the CC1101 chip is not connected
+        u8g2_t u8g2;
+        u8g2_init(&u8g2, ...); // Initialize the display
+        while (1)
+        {
+            u8g2_clear_buffer(&u8g2);
+            u8g2_draw_string(&u8g2, 0, 0, "CC1101 chip not connected.");
+            u8g2_draw_string(&u8g2, 0, 10, "Press OK to check again.");
+            u8g2_send_buffer(&u8g2);
+
+            // Wait for the user to press OK
+            if (get_user_input() == 0)
+            {
+                break;
+            }
+        }
+    }
+
+    // Initialize the STM32WB55
     stm32wb55_init();
 
-    // Initialize the U8G2 library and display the main menu
+    // Display the main menu
     u8g2_t u8g2;
     u8g2_init(&u8g2, ...); // Initialize the display
     while (1)
@@ -68,10 +114,6 @@ void select_frequency()
     {
         frequency = 868;
     }
-    else
-    {
-        // Handle invalid input
-    }
     // Set the frequency on the CC1101 chip
     cc1101_set_frequency(frequency);
 }
@@ -105,78 +147,167 @@ void send_file()
     f_read(&file, buffer, sizeof(buffer), &bytes_read);
     f_close(&file);
 
-    // Transmit the contents of the file using the CC1101 chip
+        // Transmit the contents of the file using the CC1101 chip
     cc1101_transmit(buffer, bytes_read);
 
     // Display a message to the user indicating that the transmission is complete
     u8g2_clear_buffer(&u8g2);
     u8g2_draw_string(&u8g2, 0, 0, "Transmission complete.");
     u8g2_send_buffer(&u8g2);
-    delay(1000); // Wait for 1 second before returning to the main menu
+    delay(1000);
 }
 
 void read_signals()
 {
-    // Set the recording flag to true
-    recording = true;
+    // Set the CC1101 chip to receive mode
+    cc1101_receive();
 
-    // Display a message to the user indicating that the recording has started
+    // Display a message to the user indicating that the CC1101 chip is waiting for signals
     u8g2_clear_buffer(&u8g2);
-    u8g2_draw_string(&u8g2, 0, 0, "Recording started.");
+    u8g2_draw_string(&u8g2, 0, 0, "Waiting for signals...");
+    u8g2_draw_string(&u8g2, 0, 10, "Press OK to stop recording.");
     u8g2_send_buffer(&u8g2);
 
-    // Initialize a buffer to hold the recorded signals
+    // Initialize variables for storing the received signals
     char buffer[1024];
     int bytes_read = 0;
+    recording = true;
 
-    // Keep reading signals until the recording flag is set to false
+    // Continuously receive signals until the user presses OK
     while (recording)
     {
-        // Read a signal using the CC1101 chip
-        int signal = cc1101_receive();
-        if (signal != -1)
+        // Wait for a signal to be received
+        int length = cc1101_receive();
+        if (length > 0)
         {
-            // Add the signal to the buffer
-            buffer[bytes_read] = (char)signal;
-            bytes_read++;
+            // Append the received signal to the buffer
+            cc1101_get_received_data(buffer + bytes_read, length);
+            bytes_read += length;
+        }
+
+        // Check if the user has pressed OK
+        if (get_user_input() == 0)
+        {
+            recording = false;
         }
     }
 
-    // Save the recorded signals to a file
+    // Save the received signals to a file
     save_signals(buffer, bytes_read);
 
     // Display a message to the user indicating that the recording is complete
+   
     u8g2_clear_buffer(&u8g2);
     u8g2_draw_string(&u8g2, 0, 0, "Recording complete.");
     u8g2_send_buffer(&u8g2);
-    delay(1000); // Wait for 1 second before returning to the main menu
+    delay(1000);
 }
 
 void save_signals(char *buffer, int bytes_read)
 {
-    // Use the FatFs and FreeRTOS libraries to save the recorded signals to a file
+    // Use the FatFs library to save the received signals to a file
     FRESULT res;
-    DIR dir;
-    static FILINFO fno;
-
-    // Select a file using the FatFs and FreeRTOS libraries
-    strcpy(file_name, ""); // Clear the file name
-    res = select_file_from_storage(&dir, &fno, file_name);
-    if (res != FR_OK)
-    {
-        // An error occurred while trying to select a file
-        return;
-    }
-
-    // Open the file and write the recorded signals to it
+    static char file_name[256];
+    strcpy(file_name, "signals.dat"); // Set the file name
     FIL file;
-    res = f_open(&file, file_name, FA_WRITE | FA_CREATE_ALWAYS);
+    res = f_open(&file, file_name, FA_CREATE_ALWAYS | FA_WRITE);
     if (res != FR_OK)
     {
-        // An error occurred while trying to open the file
+        // An error occurred while trying to create the file
         return;
     }
-    UINT bytes_written;
-    f_write(&file, buffer, bytes_read, &bytes_written);
+    f_write(&file, buffer, bytes_read, NULL);
     f_close(&file);
+}
+
+int get_user_input()
+{
+    // Read the user input from the OK, UP, DOWN, and BACK buttons
+    // Return 0 if OK is pressed, 1 if UP is pressed, 2 if DOWN is pressed, and 3 if BACK is pressed
+    // Add code here to read the input from the buttons
+}
+
+int cc1101_init()
+{
+    // Initialize the CC1101 chip and return 1 if successful, 0 if unsuccessful
+    // Add code here to initialize the CC1101 chip
+}
+
+int stm32wb55_init()
+{
+    // Initialize the STM32WB55 and return 1 if successful, 0 if unsuccessful
+    // Add code here to initialize the STM32WB55
+}
+
+void u8g2_init(u8g2_t *u8g2, ...)
+{
+    // Initialize the U8G2 library with the specified display controller and device arguments
+    // Add code here to initialize the U8G2 library
+}
+
+int select_file_from_storage(DIR *dir, FILINFO *fno, char *file_name)
+{
+    // Use the FatFs and FreeRTOS libraries to allow the user to select a file from the internal storage
+    // Return FR_OK if a file was successfully selected, or an error code if an error occurred
+    // Add code here to use the FatFs and FreeRTOS libraries to select a file from the internal storage
+}
+
+void cc1101_set_frequency(int frequency)
+{
+    // Set the frequency on the CC1101 chip
+    // Add code here to set the frequency on the CC1101 chip
+}
+
+void cc1101_transmit(char *buffer, int length)
+{
+    // Transmit the contents of the buffer using the CC1101 chip
+    // Add code here to transmit the contents of the buffer using the CC1101 chip
+// Pin definitions for the CC1101
+#define CC1101_CS_GPIO_Port GPIOC
+#define CC1101_CS_Pin GPIO_PIN_0
+#define CC1101_MOSI_GPIO_Port GPIOA
+#define CC1101_MOSI_Pin GPIO_PIN_7
+#define CC1101_MISO_GPIO_Port GPIOA
+#define CC1101_MISO_Pin GPIO_PIN_6
+#define CC1101_SCK_GPIO_Port GPIOA
+#define CC1101_SCK_Pin GPIO_PIN_5
+#define CC1101_GDO0_GPIO_Port GPIOC
+#define CC1101_GDO0_Pin GPIO_PIN_1
+
+SPI_HandleTypeDef hspi1;}
+
+void cc1101_receive()
+{
+    // Set the CC1101 chip to receive mode
+    // Add code here to set the CC1101 chip to receive mode
+}
+
+int cc1101_receive()
+{
+    // Receive a signal using the CC1101 chip and return the length of the received data, or 0 if no data was received
+    // Add code here to receive a signal using the CC1101 chip
+}
+
+void cc1101_get_received_data(char *buffer, int length)
+{
+    // Copy the received data from the CC1101 chip into the buffer
+    // Add code here to copy the received data from the CC1101 chip into the buffer
+}
+
+void delay(int milliseconds)
+{
+    // Delay for the specified number of milliseconds
+    // Add code here to delay for the specified number of milliseconds
+}
+
+int check_cc1101_connected()
+{
+    // Check if the CC1101 chip is connected to the correct GPIO pins and return 1 if it is, 0 if it is not
+    // Add code here to check if the CC1101 chip is connected to the correct GPIO pins
+}
+
+void show_cc1101_not_connected_message()
+{
+    // Display a message to the user indicating that the CC1101 chip is not connected and to press OK to try again
+    // Add code here to display the message and wait for the user to press OK
 }
